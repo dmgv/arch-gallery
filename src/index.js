15e4f1156838ee75f62a2633a450e6289402530a
Diego M. G. Vieira <diego.menezzes@gmail.com>
2018-07-09 15:04:01 -0300

    Add base markup and style

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("Works!");
+const rows = document.querySelector(".rows");
+const columns = document.querySelector(".columns");
+
+rows.addEventListener("click", () => {
+  rows.classList.toggle("active");
+});
+
+columns.addEventListener("click", () => {
+  columns.classList.toggle("active");
+});
