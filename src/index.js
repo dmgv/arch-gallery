4a91a01f9f6d63aef609550bef8dc4aa94fe1a45
Diego M. G. Vieira <diego.menezzes@gmail.com>
2018-07-19 16:36:33 -0300

    Add js to create gallery

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
-const rows = document.querySelector(".rows");
-const columns = document.querySelector(".columns");
+const getRandomNum = () => Math.floor(Math.random() * 447);
 
-rows.addEventListener("click", () => {
-  rows.classList.toggle("active");
-});
-
-columns.addEventListener("click", () => {
-  columns.classList.toggle("active");
-});
-
-// const janela = () => {
-//   console.log("clicou");
-// };
-
-function janela() {
-  console.log("clicou");
+function generateHTML() {
+  return `
+  <a href="">
+    <img src="https://source.unsplash.com/collection/762960/${getRandomNum()}" alt=""/>
+  </a>
+  `;
 }
+
+const gallery = document.querySelector("main");
+const digits = Array.from({ length: 16 });
+const html = digits.map(generateHTML).join("");
+gallery.innerHTML = html;
